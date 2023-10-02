8a7038c719b8925bbc1b5eaacd5e4ce5ea678e2b
paulabeatrizsoliveira <biamillez@gmail.com>
2023-10-02 11:16:40 -0300

    pagina1

---
+++ validaForm.js
@@ -0,0 +1,18 @@
+document
+   .getElementById("loginForm")
+   .addEventListener("submit", function (event) {
+      event.preventDefault(); // Impede o envio do formulário padrão
+
+      // Obter os valores dos campos
+      var codigo = document.getElementById("codigo").value;
+      var senha = document.getElementById("senha").value;
+
+      // Lógica de validação 
+      if (codigo === "adm123" && senha === "adm123") {
+         // Redirecionar para outra página após a validação
+         window.location.href =
+            "https://juliafclima.github.io/dashboardJ-paula/pages/index.html";
+      } else {
+         alert("Código ou senha inválidos. Tente novamente.");
+      }
+   });
